16ec27749a949d7b451606bad43274d362fc6850
Lilly Piri <lilly@lillypiri.com>
2017-08-08 14:06:39 +1000

    key added

--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -5,7 +5,7 @@ const GifList = props => {
 
   const results = props.data;
   let gifs = results.map(gif =>
-    <Gif url={gif.images.fixed_height.url}/>
+    <Gif url={gif.images.fixed_height.url} key={gif.id} />
   );
   return(
     <ul className="gif-list">
