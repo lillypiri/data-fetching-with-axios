36ba41f5ecdb01d1beba8e80e4328b1bd2684e38
Lilly Piri <lilly@lillypiri.com>
2017-08-08 14:20:18 +1000

    returns default search on loading

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ export default class App extends Component {
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.performSearch();
+  }
 
-  performSearch = (query) => {
+  performSearch = (query = 'cats') => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -12,7 +12,7 @@ export default class SearchForm extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.props.onSearch(this.state.searchText);
+    this.props.onSearch(this.query.value);
     e.currentTarget.reset();
   }
 
@@ -23,6 +23,7 @@ export default class SearchForm extends Component {
         <input type="search"
                onChange={this.onSearchChange}
                name="search"
+               ref={(input) => this.query = input}
                placeholder="Search..." />
         <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
       </form>
