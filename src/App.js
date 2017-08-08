fc18c1d4381ba5439fe49719e49545a977da60f7
Lilly Piri <lilly@lillypiri.com>
2017-08-08 14:28:52 +1000

    added loading message when page is loading gifs - replaces error message which was previously showing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     };
   }
 
@@ -20,7 +21,8 @@ export default class App extends Component {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
-          gifs: response.data.data
+          gifs: response.data.data,
+          loading: false
         });
       })
       .catch(error => {
@@ -39,7 +41,11 @@ export default class App extends Component {
           </div>
         </div>
         <div className="main-content">
-          <GifList data={this.state.gifs} />
+          {
+            (this.state.loading)
+            ? <p>Loading...</p>
+            : <GifList data={this.state.gifs} />
+          }
         </div>
       </div>
     );
