9277a8e31480e519cf9f670550001e7eb96a0201
Lilly Piri <lilly@lillypiri.com>
2017-08-08 14:25:21 +1000

    added error message when query returns no results

--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Gif from './Gif';
+import NoGifs from './NoGifs';
 
 const GifList = props => {
 
   const results = props.data;
-  let gifs = results.map(gif =>
-    <Gif url={gif.images.fixed_height.url} key={gif.id} />
-  );
+  let gifs;
+  if (results.length > 0) {
+    gifs = results.map(gif => <Gif url={gif.images.fixed_height.url} key={gif.id} />);
+  } else {
+    gifs = <NoGifs />
+  }
+
   return(
     <ul className="gif-list">
       {gifs}
