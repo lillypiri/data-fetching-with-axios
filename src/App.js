6bb55e6003fe5d217089194727d41dbd5f521df3
Lilly Piri <lilly@lillypiri.com>
2017-08-08 14:14:48 +1000

    search functionality

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ export default class App extends Component {
     };
   }
 
-  componentDidMount() {
-    axios.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC")
+  componentDidMount() {}
+
+  performSearch = (query) => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
           gifs: response.data.data
@@ -22,7 +24,7 @@ export default class App extends Component {
       .catch(error => {
         console.log("Error fetching and parsing data", error);
       });
-  }
+  };
 
   render() {
     console.log(this.state.gifs);
@@ -31,7 +33,7 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />
+            <SearchForm onSearch={this.performSearch} />
           </div>
         </div>
         <div className="main-content">
